b2d3c802a3470ebe13937862d7c3c05aa6d63feb
showeimer <showeimer@gmail.com>
2017-08-21 10:55:01 -0400

    fixed main.js errors with needed code that was commented out

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Dog Constructor & Prototype
 function Dog (name, status, color, hungry, owner) {
-  // this.name = name;
+  this.name = name;
   this.status = status || 'normal';
   this.color = color;
   this.hungry = hungry || false;;
@@ -9,7 +9,7 @@ function Dog (name, status, color, hungry, owner) {
 
 // Human Constructor & Prototype
 function Human (name, cool) {
-  // this.name = name;
+  this.name = name;
   this.pet = function(dog) {
     dog.status = 'happy';
   }
@@ -27,7 +27,3 @@ let atticus = new Dog('Atticus')
 
 let mason = new Human('Mason', false);
 let julia = new Human('Julia', true);
-
-sadie.owner = mason;
-
-// julia.feed(moonshine);
